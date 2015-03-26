5865b8ee6e8cfd109033a8316ad34e69fc2b5d87
Muhammad Aisamuddin Eka Pramudita <isam.muhammad@ymail.com>
2015-03-26 19:24:31 +0000

    Revert "Done"
    This reverts commit 403e7273006af2570e7e99a13c470db3085e6301.

--- Tugas 2/delapan.java
+++ Tugas 2/delapan.java
@@ -1,17 +1,14 @@
-public class delapan
-{
-    public static void main(String[] args) 
-    {
+public class delapan{
+    public static void main(String[] args) {
         int bg=110;
     System.out.println("*Hasil Urutan Angka 1-110*");
     System.out.println("*Ganti Baris Setiap 11 Angka*");
     for (int i=1; i<=bg; i++)
-        {
-            System.out.print(i+" ");
-            if (i%11==0) 
-            {
-            System.out.println(" ");
-            }
+    {
+        System.out.print(i+" ");
+        if (i%11==0) {
+            System.out.println("");
+        }
         }
     }
 }
