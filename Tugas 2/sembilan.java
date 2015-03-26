825302d7306aa3b6f3817da31713c7159f02c7b6
Muhammad Aisamuddin Eka Pramudita <isam.muhammad@ymail.com>
2015-03-26 19:23:10 +0000

    Tugas 2
    yang sebelumnya kurang perbaris harus 11 angka

--- Tugas 2/sembilan.java
+++ Tugas 2/sembilan.java
@@ -1,19 +1,17 @@
 public class sembilan{
     public static void main(String[] args) 
     {
-        int bg=110;
     System.out.println("*Mengurutkan Angka*");
-    for (int i=1; i<bg; i++)
+    for (int i=1; i<=110; i++)
         {
-        if (i%5==0) 
+            System.out.print(i+" ");
+            if (i%11==0) 
             {
-            System.out.print("JONI ");
+            System.out.println(" ");
             }
-        if (i%5==0)continue;
-            { 
-            System.out.print(i);
-            System.out.print(" ");
-            }
+            else if (i%5==0) {
+                System.out.print("JONI ");
+            }continue;
         }       
     }
 }
